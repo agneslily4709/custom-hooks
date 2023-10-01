20945c25739197648514f22e8518bf84c7c4ecd0
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-10-01 23:55:52 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import useCounter from './hooks/useCounter';
+
+function App() {
+        const [count,increment,decrement] = useCounter(0);
+        return (
+          <div>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+          </div>
+        )
+}
+
+export default App;
